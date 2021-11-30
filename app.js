5d4ad52145447f106c6418c69dec5f197722a7e3
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-11-29 16:39:49 -0800

    undid all changes because I didn't understand a single thing

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const boulderContainer = document.getElementById('boulder-container');
 
 const totalEl = document.getElementById('total');
 const winsEl = document.getElementById('wins');
+const lossEl = document.getElementById('loss');
 
 // initialize state
 const hidingPlaces = [
@@ -28,25 +29,15 @@ function getRandomItem(arr) {
 
 function handleGuess(userGuess, correctSpot) {
     // should reset the styles
-    resetStyles();
     // then increment the guesses
-    totalGuesses++;
+
     // then grab the appropriate container element for the correct guess from the DOM
-    const winningElem 
+    
     // then add the face class to that element so that the face shows up
     // winningElem.src = './assets/'; WHERE????
     // then if the user guess is correct, increment the correct guesses
-    if (winningElem === '') {
-        winsEl++;
-    }
-    // update the DOM to show this change to the user (including the losses, not tracked directly in state)
-    displayWinLossTotal();
-}
 
-function displayWinLossTotal() {
-    winsEl.textContent = wins;
-    lossEl.textContent = total - wins;
-    totalEl.textContent = total;
+    // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 }
 
 function resetStyles() {
@@ -55,18 +46,16 @@ function resetStyles() {
 
 shedButton.addEventListener('click', () => {
     // should get a random item to call the 'correct spot'
-    handleGuess('shed');
     // then use that correct spot to 'handle the guess' using the handleGuess function
 });
 
 treeButton.addEventListener('click', () => {
     // should get a random item to call the 'correct spot'
-    handleGuess('tree');
     // then use that correct spot to 'handle the guess' using the handleGuess function
 });
 
 boulderButton.addEventListener('click', () => {
     // should get a random item to call the 'correct spot'
-    handleGuess('boulder');
+
     // then use that correct spot to 'handle the guess' using the handleGuess function
 });
